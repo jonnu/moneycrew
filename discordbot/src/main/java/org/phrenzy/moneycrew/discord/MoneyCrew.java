a23fd9c4efd38ca28b5cdc7071433d432de8d0b9
jonnu <code@jonnu.eu>
2021-08-21 19:35:15 +0100

    allow token to be supplied via environment variable

--- discordbot/src/main/java/org/phrenzy/moneycrew/discord/MoneyCrew.java
+++ discordbot/src/main/java/org/phrenzy/moneycrew/discord/MoneyCrew.java
@@ -8,30 +8,45 @@ import org.javacord.api.util.logging.ExceptionLogger;
 import org.phrenzy.moneycrew.discord.di.DiscordModule;
 import org.phrenzy.moneycrew.discord.scrim.service.MessageListener;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class MoneyCrew {
 
+    private static final char ARG_SEPARATOR = '=';
     private static final String ENV_TOKEN = "TOKEN";
 
     public static void main(final String[] args) {
-
-        if (args.length == 0) {
-            throw new IllegalStateException("DiscordApi token required, but was not passed.");
-        }
 
         final Injector injector = Guice.createInjector(Stage.PRODUCTION, ImmutableList.of(new DiscordModule()));
         final MessageListener<DiscordApi> listener = injector.getInstance(Key.get(new TypeLiteral<MessageListener<DiscordApi>>() {}));
 
         // Read the token from env (or the first program parameter) when invoking the bot.
-        final String token = Optional.ofNullable(System.getenv(ENV_TOKEN))
-                .orElse(args[0].substring(args[0].indexOf('=') + 1));
+        final String token = getTokenFromEnvironment()
+                .or(() -> getTokenFromArguments(args))
+                .orElseThrow(() -> new IllegalArgumentException("DiscordApi token missing"));
 
         new DiscordApiBuilder()
                 .setToken(token)
                 .login()
                 .thenAccept(listener::bindListeners)
                 .exceptionally(ExceptionLogger.get());
+    }
+
+    private static Optional<String> getTokenFromEnvironment() {
+        return Optional.ofNullable(System.getenv(ENV_TOKEN));
+    }
+
+    private static Optional<String> getTokenFromArguments(final String[] args) {
+
+        if (args == null || args.length == 0) {
+            return Optional.empty();
+        }
+
+        return Arrays.stream(args)
+                .filter(arg -> arg.indexOf(ARG_SEPARATOR) != -1)
+                .map(arg -> arg.substring(arg.indexOf(ARG_SEPARATOR) + 1))
+                .findFirst();
     }
 
 }
