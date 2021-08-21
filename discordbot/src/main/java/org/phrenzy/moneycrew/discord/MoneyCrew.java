730de13ce36729b7c1f66288a05d7a24ef58c3e6
jonnu <code@jonnu.eu>
2021-08-21 19:23:33 +0100

    allow token to be supplied via environment variable

--- discordbot/src/main/java/org/phrenzy/moneycrew/discord/MoneyCrew.java
+++ discordbot/src/main/java/org/phrenzy/moneycrew/discord/MoneyCrew.java
@@ -8,7 +8,11 @@ import org.javacord.api.util.logging.ExceptionLogger;
 import org.phrenzy.moneycrew.discord.di.DiscordModule;
 import org.phrenzy.moneycrew.discord.scrim.service.MessageListener;
 
+import java.util.Optional;
+
 public class MoneyCrew {
+
+    private static final String ENV_TOKEN = "TOKEN";
 
     public static void main(final String[] args) {
 
@@ -16,18 +20,18 @@ public class MoneyCrew {
             throw new IllegalStateException("DiscordApi token required, but was not passed.");
         }
 
-        Injector injector = Guice.createInjector(Stage.PRODUCTION, ImmutableList.of(new DiscordModule()));
-        MessageListener<DiscordApi> listener = injector.getInstance(Key.get(new TypeLiteral<MessageListener<DiscordApi>>() {}));
+        final Injector injector = Guice.createInjector(Stage.PRODUCTION, ImmutableList.of(new DiscordModule()));
+        final MessageListener<DiscordApi> listener = injector.getInstance(Key.get(new TypeLiteral<MessageListener<DiscordApi>>() {}));
 
-        // Read the token from the first program parameter when invoking the bot
-        String token = args[0].substring(args[0].indexOf('=') + 1);
+        // Read the token from env (or the first program parameter) when invoking the bot.
+        final String token = Optional.ofNullable(System.getenv(ENV_TOKEN))
+                .orElse(args[0].substring(args[0].indexOf('=') + 1));
 
         new DiscordApiBuilder()
                 .setToken(token)
                 .login()
                 .thenAccept(listener::bindListeners)
-                .exceptionally(ExceptionLogger.get())
-                .join();
+                .exceptionally(ExceptionLogger.get());
     }
 
 }
